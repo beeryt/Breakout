e0d1fee0def77aa08e9af56248278b8289f52370
Carl B. Smiley <beeryt@noreply.users.github.com>
2020-05-06 11:59:43 -0700

    feat: add brick destruction

--- src/Brick.gd
+++ src/Brick.gd
@@ -1,5 +1,5 @@
 tool
-extends Area2D
+extends StaticBody2D
 
 export var size := Vector2(50,20) setget set_size
 export var color := Color(1,1,0) setget set_color
@@ -38,6 +38,6 @@ func set_color(value):
 	update()
 
 
-func _on_Brick_body_shape_entered(_body_id, _body, _body_shape, _area_shape):
+func destroy():
 	emit_signal("brick_destroyed")
 	queue_free()
